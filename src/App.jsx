54aa4ef410570dd3a1c163faa201b0ac7285caaf
RicardoMejolaro <ricardo.mejolaro@gmail.com>
2021-11-08 22:01:13 -0300

    project-structure-with-reload-webpack-and-sass

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import styles from './styles/global.scss';
+
+export function App() {
+    return (
+        <h1>Reload Webpack</h1>
+    )
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -9,6 +14,18 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx']
     },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public'),
+        },
+        compress: true,
+        port: 8080,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
     module: {
         rules: [
             {
@@ -16,6 +33,11 @@ module.exports = {
                 exclude: /node_modules/,
                 use: 'babel-loader',
             },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
         ]
     }
 }
