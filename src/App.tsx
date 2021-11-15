635289891e4361429c29aeb0ca68f504ac7ae477
RicardoMejolaro <ricardo.mejolaro@gmail.com>
2021-11-15 20:09:16 -0300

    tychanged extensions to tsx and created component types

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { RepositoryList } from './components/RepositoryList';
 
 //Estilos
-import styles from './styles/global.scss';
+import './styles/global.scss';
 
 export function App() {
     return (
